21546e6cbfc0d7e33c41e99aefc9b2659d6a89cf
Fabio Cortes <fcor14@hotmail.com>
2019-10-10 12:13:42 -0500

    Fading between routes :)

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,14 +1,19 @@
 import * as React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import Header from "../Header";
 import Footer from "../Footer";
 import { ILayoutProps } from "./types";
 import "./styles.scss";
 
-const Layout = ({ children }: ILayoutProps) => (
+const Layout = ({ children, location }: ILayoutProps) => (
   <div className="app">
     <div className="content">
       <Header />
-      {children}
+      <TransitionGroup exit={false}>
+        <CSSTransition key={location.pathname.split("/")[1]} timeout={250} classNames="fade">
+          {children}
+        </CSSTransition>
+      </TransitionGroup>
     </div>
     <Footer />
   </div>
