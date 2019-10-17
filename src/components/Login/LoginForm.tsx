c6b5cf3fe6fc35a7114e65f5f4c941bd2f3c442a
Fabio Cortes <fcor14@hotmail.com>
2019-10-17 18:42:53 -0500

    basic UI

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,65 @@
+import * as React from "react";
+import "./styles.scss";
+import { Formik } from "formik";
+import Button from "../Common/Button/";
+
+const LoginForm = () => (
+  <div>
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      validate={(values) => {
+        const errors = {
+          email: "",
+        };
+        if (!values.email) {
+          errors.email = "Required";
+        } else if (
+          !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+        ) {
+          errors.email = "Invalid email address";
+        }
+        return errors;
+      }}
+      onSubmit={(values, { setSubmitting }) => {
+        setTimeout(() => {
+          alert(JSON.stringify(values, null, 2));
+          setSubmitting(false);
+        }, 400);
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        isSubmitting,
+      }) => (
+        <form onSubmit={handleSubmit} className="column">
+          <input
+            type="email"
+            name="email"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.email}
+          />
+          {errors.email && touched.email && errors.email}
+          <input
+            type="password"
+            name="password"
+            onChange={handleChange}
+            onBlur={handleBlur}
+            value={values.password}
+          />
+          {errors.password && touched.password && errors.password}
+          <Button variant="rounded">
+            Inicia sesión
+          </Button>
+        </form>
+      )}
+    </Formik>
+  </div>
+);
+
+export default LoginForm;
--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,5 +1,26 @@
 import * as React from "react";
+import "./styles.scss";
+import LoginForm from "./LoginForm";
 
-const Login = () => <div>Acá va el Login</div>;
+const Login = () => {
+  return (
+    <div className="login-container">
+      <div className="login-card column">
+        <img
+          src="/assets/dummy3.png"
+          alt="login_img"
+          width="96"
+        />
+        <p>¡Bienvenido!</p>
+        <LoginForm />
+        <p>¿Eres nuevo? ¡No te preocupes!</p>
+        <p>
+          Crea tu cuenta
+          <span>aquí</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
