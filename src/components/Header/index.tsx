f88eb3f77f1d3bb615a0da0fbc0992d0f9fb4009
Fabio Cortés <fcor14@hotmail.com>
2019-11-01 18:43:34 -0500

    Basic header link validation (#13)

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,9 +2,44 @@ import * as React from "react";
 import { NavLink } from "react-router-dom";
 import { IHeaderProps} from "./types";
 import Enter from "../Icons/Enter";
+import User from "../Icons/User";
 import "./styles.scss";
+import { IAppState } from "../../store/types";
+import { useSelector as useMappedState } from "react-redux";
+
+const selector = (state: IAppState) => {
+  return {
+    isAuthenticated: state.profile.isLoggedIn,
+  };
+};
+
+const noAuthLiknks = [
+  {
+    text: "Login",
+    icon: <Enter />,
+    link: "/login",
+  },
+];
+
+const authLiknks = [
+  {
+    text: "Profile",
+    icon: <User width={17.68} height={19.24} />,
+    link: "/profile",
+  },
+];
 
 const Header = ({ location }: IHeaderProps) => {
+  const { isAuthenticated } = useMappedState(selector);
+
+  const links = React.useMemo(
+    () => {
+      if (isAuthenticated) {
+        return authLiknks;
+      }
+      return noAuthLiknks;
+    }, [isAuthenticated],
+  );
 
   return (
     <header className="row container">
@@ -15,11 +50,17 @@ const Header = ({ location }: IHeaderProps) => {
           width="150"
         />
       </NavLink>
-
-      <NavLink to="/login" className="row link-container" >
-        <Enter />
-        <p>Login</p>
-      </NavLink>
+      <div className="header-links row">
+        {links.map((link) => {
+          return (
+            <NavLink to={link.link} className="row link-container" >
+              {link.icon}
+              <p>{link.text}</p>
+            </NavLink>
+          );
+        })
+        }
+      </div>
     </header>
   );
 };
