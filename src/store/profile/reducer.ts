71b57f1dc5a23045110f5dfc8a095c384f49d47f
Fabio Cortes <fcor14@hotmail.com>
2019-11-16 11:10:57 -0500

    Fixed public path webpack config

--- src/store/profile/reducer.ts
+++ src/store/profile/reducer.ts
@@ -3,7 +3,7 @@ import { IProfileReducerState } from "./types";
 import { fetchProfile, login, logout } from "./actions";
 
 const initialState: IProfileReducerState = {
-  isLoggedIn: false,
+  isLoggedIn: true,
   isLoadingProfile: false,
   user: undefined,
 };
